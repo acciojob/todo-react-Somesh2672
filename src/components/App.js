7955341883344a87207f09da59239d8604f5dcb9
Somesh Jaiswal <someshjaiswal98@gmail.com>
2023-09-16 00:26:31 +0530

    assisgnment 1.2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,64 @@
+// import React,{useState} from "react";
+// import './../styles/App.css';
 
-import React from "react";
-import './../styles/App.css';
+// const [toDoList,setToDoList] = useState("")
+
+// const App = () => {
+//   const addToDo =()=>{
+
+//   }
+//   return (
+//     <div>
+//         <h1>To Do List</h1>
+//         <input type="text" id="textInput" placeholder="Add item"></input>
+//         <button type="button" onClick={addToDo}>To Do</button>
+
+//     </div>
+//   )
+// }
+
+// export default App;
+
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [toDoList, setToDoList] = useState([]);
+  const [task, setTask] = useState("");
+
+  const addToDo = () => {
+    if (task.trim() !== "") {
+      setToDoList([...toDoList, task]);
+      setTask("");
+    }
+  };
+
+  const handleTaskChange = (event) => {
+    setTask(event.target.value);
+  };
+
+
+  const deleteToDo = (index) => {
+    const updatedList = [...toDoList];
+    updatedList.splice(index, 1);
+    setToDoList(updatedList);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>To Do List</h1>
+      <input type="text" id="textInput" placeholder="Add item" value={task} onChange={handleTaskChange}/>
+      <button type="button" onClick={addToDo}> Add To Do </button>
+      <ul>
+        {toDoList.map((item, index) => (
+          <li key={index} > 
+            {item} <button onClick={() => deleteToDo(index)}>Delete</button>
+          
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
